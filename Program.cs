c4f5df50d890af3823c99993488835395e00f271
Kiritmak <iran07ma@gmail.com>
2025-05-02 01:38:03 -0400

    Implementadas mejoras visuales, reorganizacion de codigo y guardado y generado de tableros a traves de ChessBoard.txt. Concretamente, se ha implementado la rotacion 180 grados del tablero para crear la vista de cada jugador.

--- Program.cs
+++ Program.cs
@@ -3,9 +3,10 @@
   static class Program
   {
     static string ColorDataFilepath = @"E:\Code\C#\LocalParalelChess\TextFiles\ColorData.txt";
-    static int CurrentPlayers = 0;
+    static string chessBoardFilepath = @"E:\Code\C#\LocalParalelChess\TextFiles\ChessBoard.txt";
     static object PlayerEnteringSala = new object();
     static string[] players = new string[2];
+    static string? playerName;
 
     private static Semaphore SalaDeEspera = new(2, 2, "SalaDeEspera");
     private static Mutex game = new(false, "ChessGame");
@@ -15,6 +16,7 @@
       //args = new string[1] {"Kiritmak"};
       Thread MainThread = new(new ThreadStart(WelcomePlayer));
       MainThread.Name = args[0];
+      playerName = MainThread.Name;
       MainThread.Start();
     }
 
@@ -29,8 +31,6 @@
         Console.WriteLine($"{playerName} Ha entrado a la sala");
         Println(playerName, "Esta esperando para pedir un color...");
         ColorSelection.WaitOne();
-        Println(playerName, "Esta eligiendo un color");
-        Console.ReadLine();
         List<string> text = File.ReadAllLines(ColorDataFilepath).ToList();
         string[] Blancas = text[0].Split(",");
         if (Blancas.Length==1)
@@ -57,35 +57,32 @@
 
     static void Play(bool blancas)
     {
-      string? playerName = Thread.CurrentThread.Name;
       string color = blancas ? "blancas" : "negras";
-      List<string> chessBoard = new List<string>();
-      for(int i=1; i<=8; i++)
-      {
-        string? line = "";
-        for(int j=1; j<=8; j++)
-        {
-          if ((i + j) % 2 == 0) line += " /// ";
-          else line += "     ";
-          line += "|";
-        }
-        chessBoard.Add(line);
-        chessBoard.Add("--------------------------------------------------");
-      }
+
+      List<string> chessBoard;
 
       while(true) 
       {
+
+
         Println(playerName, "Esperando para elejir...");
         game.WaitOne();
         Println(playerName, "Es tu turno");
-        int w = Win(blancas);
-        if (w != -1) break;
-        showChessBoard(chessBoard);
+
+        chessBoard = File.ReadAllLines(chessBoardFilepath).ToList();
+        if (Win(blancas)!= -1) break;
+        showChessBoard(chessBoard, blancas);
+
         Println(playerName, "Esta pensando... ");
-        Console.ReadLine();
+        Console.ReadLine(); 
+        //Leer el movimiento del jugador y, elejir si es valido o no
+        //Transformar el tablero
+        //Guardar la posicion
         Println(playerName, "Ha elejido un movimiento");
+
         game.ReleaseMutex();
-        Thread.Sleep(2000);
+        do Thread.Sleep(2000);
+        while (File.ReadAllLines(ColorDataFilepath)[1].Split(',').ToList().Count == 1);
       } 
 
     }
@@ -111,13 +108,88 @@
       Console.WriteLine($"{name} {message}");
     static int Win(bool blancas)
     {
-      return -1;
+      int result = -1;
+      if(result!=-1) //Reseting the board to original state
+      {
+        List<string> chessBoard = new List<string>();
+        for (int i = 0; i < 8; i++)
+        {
+          string line = "";
+          for (int j = 0; j < 8; j++)
+          {
+            if ((i + j) % 2 == 0) line += "W";
+            else line += "B";
+          }
+          chessBoard.Add(line);
+        }
+        File.WriteAllLines(chessBoardFilepath, chessBoard);
+      }
+      if (result == 0) //Ganan las blancas
+      {
+        if (blancas) Println(playerName, "Ha ganado. Felicidades");
+        else Println(playerName, "Ha perdido. Suerte Para la proxima");
+      }
+      else if (result == 1) //Ganan las negras
+      {
+        if (blancas) Println(playerName, "Ha perdido. Suerte Para la proxima");
+        else Println(playerName, "Ha ganado. Felicidades");
+      }
+      return result;
     }
-    static void showChessBoard(List<string> chessBoard)
+    static void showChessBoard(List<string> chessBoard, bool blancas)
     {
+      List<string> showBoard = new List<string>();
+      int line = 1;
+
+      showBoard.Add("-------------------------------------------------");
       foreach (string chess in chessBoard)
       {
-        Console.WriteLine(chess);
+        string s = "";
+        foreach (char c in chess)
+        {
+          string view;
+          switch (c)
+          {
+            case 'B': view = "   "; break;
+            case 'W': view = "///"; break;
+            default: throw new NotImplementedException();
+          }
+          view = $"| {view} ";
+          s+=(view);
+        }
+        s+="|";
+        showBoard.Add(s);
+        showBoard.Add("-------------------------------------------------");
+      }
+
+      string s2 = "";
+      char colom = 'A';
+      Func<char, bool> condition = (char c) => { return c<='H'; };
+      if (!blancas)
+      {
+        condition = (char c) => { return c >= 'A'; };
+        colom = 'H';
+        line = 8;
+      }
+
+      for (char c = colom; condition(c);)
+      {
+        s2 += $"   {c}  ";
+        if (blancas) c++;
+        else c--;
+      }
+      showBoard.Add(s2);
+
+      for(int i=0; i<showBoard.Count; i++) 
+      {
+        if (showBoard[i][0] != '-' && i!=showBoard.Count-1)
+        {
+          Console.Write($"{line} ");
+          line += blancas ? 1 : -1;
+        }
+        
+        else Console.Write("  ");
+        Console.WriteLine(showBoard[i]);
       }
     }
   }
