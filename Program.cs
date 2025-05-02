f5012c4afe1adc93da3ecdd8f38a8110a328df2e
Kiritmak <iran07ma@gmail.com>
2025-05-01 23:25:44 -0400

    Implementada una secuencia turnos. Se mostrara el tablero antes de tomar una decision.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
   static class Program
   {
-    static string filepath = @"E:\Code\C#\LocalParalelChess\TextFiles\ColorData.txt";
+    static string ColorDataFilepath = @"E:\Code\C#\LocalParalelChess\TextFiles\ColorData.txt";
     static int CurrentPlayers = 0;
     static object PlayerEnteringSala = new object();
     static string[] players = new string[2];
@@ -21,8 +21,6 @@
     static void WelcomePlayer()
     {
       string? playerName = Thread.CurrentThread.Name;
-
-
       Console.WriteLine($"Welcome to Paralel Chess {playerName}!\nA game where you can play chess with your friend at separated windows in the same computer!\n");
       while (Exit())
       {
@@ -33,24 +31,25 @@
         ColorSelection.WaitOne();
         Println(playerName, "Esta eligiendo un color");
         Console.ReadLine();
-        List<string> text = File.ReadAllLines(filepath).ToList();
+        List<string> text = File.ReadAllLines(ColorDataFilepath).ToList();
         string[] Blancas = text[0].Split(",");
         if (Blancas.Length==1)
         {
           Blancas = ["Blancas", playerName] ;
           text[0] = $"Blancas, {playerName}";
-          File.WriteAllLines(filepath, text);
+          File.WriteAllLines(ColorDataFilepath, text);
         }
         else
         {
           text[1] = $"Negras, {playerName}";
-          File.WriteAllLines(filepath, text);
+          File.WriteAllLines(ColorDataFilepath, text);
         }
+        Println(playerName, "Ha elejido un color");
         ColorSelection.ReleaseMutex();
         Play(Blancas[1] == playerName);
         text[0] = "Blancas";
         text[1] = "Negras";
-        File.WriteAllLines (filepath, text);
+        File.WriteAllLines (ColorDataFilepath, text);
         Console.WriteLine($"{playerName} Ha salido de la sala");
         SalaDeEspera.Release();
       }
@@ -60,8 +59,35 @@
     {
       string? playerName = Thread.CurrentThread.Name;
       string color = blancas ? "blancas" : "negras";
-      Console.WriteLine($"{playerName} esta jugando con las {color}");
-      Console.ReadLine();
+      List<string> chessBoard = new List<string>();
+      for(int i=1; i<=8; i++)
+      {
+        string? line = "";
+        for(int j=1; j<=8; j++)
+        {
+          if ((i + j) % 2 == 0) line += " /// ";
+          else line += "     ";
+          line += "|";
+        }
+        chessBoard.Add(line);
+        chessBoard.Add("--------------------------------------------------");
+      }
+
+      while(true) 
+      {
+        Println(playerName, "Esperando para elejir...");
+        game.WaitOne();
+        Println(playerName, "Es tu turno");
+        int w = Win(blancas);
+        if (w != -1) break;
+        showChessBoard(chessBoard);
+        Println(playerName, "Esta pensando... ");
+        Console.ReadLine();
+        Println(playerName, "Ha elejido un movimiento");
+        game.ReleaseMutex();
+        Thread.Sleep(2000);
+      } 
+
     }
     static bool Exit()
     {
@@ -83,6 +109,17 @@
     }
     static void Println(string? name, string message) =>
       Console.WriteLine($"{name} {message}");
+    static int Win(bool blancas)
+    {
+      return -1;
+    }
+    static void showChessBoard(List<string> chessBoard)
+    {
+      foreach (string chess in chessBoard)
+      {
+        Console.WriteLine(chess);
+      }
+    }
   }
 
 }
