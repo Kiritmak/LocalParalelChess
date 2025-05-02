4211a0b587cecd78773354fb152c2635caff2010
Kiritmak <iran07ma@gmail.com>
2025-05-01 20:42:41 -0400

    Creada una sala de espera de maximo 2 personas

--- Program.cs
+++ Program.cs
@@ -3,22 +3,58 @@
   static class Program
   {
     static int CurrentPlayers = 0;
-    static void Main(string playerName)
+    static object PlayerEnteringSala = new object();
+    static string[] players = new string[2];
+
+    private static Semaphore SalaDeEspera = new(2, 2, "SalaDeEspera");
+    private static Mutex game = new(false, "ChessGame");
+    static void Main(string[] args)
     {
-      Mutex game = new Mutex(false, "ChessGame");
-      Console.WriteLine($"Welcome to Paralel Chess!\nA game where you can play chess with your friend at separated windows in the same computer!");
+      //args = new string[1] {"Kiritmak"};
+      Thread MainThread = new(new ThreadStart(WelcomePlayer));
+      MainThread.Name = args[0];
+      MainThread.Start();
+    }
+
+    static void WelcomePlayer()
+    {
+      string? playerName = Thread.CurrentThread.Name;
+      Console.WriteLine($"Welcome to Paralel Chess {playerName}!\nA game where you can play chess with your friend at separated windows in the same computer!\n");
       while (Exit())
       {
-        //Joing the game
+        Println(playerName, "Esta esperando ...");
+        SalaDeEspera.WaitOne();
+        Console.WriteLine($"{playerName} Ha entrado a la sala");
+        Console.ReadLine();
+        Console.WriteLine($"{playerName} Ha salido de la sala");
+        SalaDeEspera.Release();
       }
     }
 
+    static void Play(bool blancas)
+    {
+
+    }
     static bool Exit()
     {
       Console.WriteLine("Type 1 to enter to the game, type anything else to exit the game");
-      int option = Console.Read();
-      return option == 1;
+      while(true)
+      {
+        int option;
+        try
+        {
+          option = int.Parse(Console.ReadLine());
+        }
+        catch
+        {
+          Console.WriteLine("Incorrect Format");
+          continue;
+        }
+        return option == 1;
+      }
     }
+    static void Println(string? name, string message) =>
+      Console.WriteLine($"{name} {message}");
   }
 
 }
