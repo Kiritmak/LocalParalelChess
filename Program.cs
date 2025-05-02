01ef7ba88794d9fa6653a2ce098a525f1d666c36
Kiritmak <iran07ma@gmail.com>
2025-05-01 22:04:20 -0400

    Implementado un sistema de deteccion de jugadores. El primero en intentar elejir color sera las blancas, el segundo las negras. A partir de aqui, la informacion sobre la partida se ira guardando en archivos de texto situados en la carpeta TextFiles. Esto es una solucion que se le ha dado al no poder acceder desde un ejecutable a las variables del otro. Los archivos de texto actuaran como variables 'ultra globales'.

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@
 {
   static class Program
   {
+    static string filepath = @"E:\Code\C#\LocalParalelChess\TextFiles\ColorData.txt";
     static int CurrentPlayers = 0;
     static object PlayerEnteringSala = new object();
     static string[] players = new string[2];
 
     private static Semaphore SalaDeEspera = new(2, 2, "SalaDeEspera");
     private static Mutex game = new(false, "ChessGame");
+    private static Mutex ColorSelection = new(false, "Color");
     static void Main(string[] args)
     {
       //args = new string[1] {"Kiritmak"};
@@ -19,13 +21,36 @@
     static void WelcomePlayer()
     {
       string? playerName = Thread.CurrentThread.Name;
+
+
       Console.WriteLine($"Welcome to Paralel Chess {playerName}!\nA game where you can play chess with your friend at separated windows in the same computer!\n");
       while (Exit())
       {
-        Println(playerName, "Esta esperando ...");
+        Println(playerName, "Esta esperando para entrar a la sala...");
         SalaDeEspera.WaitOne();
         Console.WriteLine($"{playerName} Ha entrado a la sala");
+        Println(playerName, "Esta esperando para pedir un color...");
+        ColorSelection.WaitOne();
+        Println(playerName, "Esta eligiendo un color");
         Console.ReadLine();
+        List<string> text = File.ReadAllLines(filepath).ToList();
+        string[] Blancas = text[0].Split(",");
+        if (Blancas.Length==1)
+        {
+          Blancas = ["Blancas", playerName] ;
+          text[0] = $"Blancas, {playerName}";
+          File.WriteAllLines(filepath, text);
+        }
+        else
+        {
+          text[1] = $"Negras, {playerName}";
+          File.WriteAllLines(filepath, text);
+        }
+        ColorSelection.ReleaseMutex();
+        Play(Blancas[1] == playerName);
+        text[0] = "Blancas";
+        text[1] = "Negras";
+        File.WriteAllLines (filepath, text);
         Console.WriteLine($"{playerName} Ha salido de la sala");
         SalaDeEspera.Release();
       }
@@ -33,7 +58,10 @@
 
     static void Play(bool blancas)
     {
-
+      string? playerName = Thread.CurrentThread.Name;
+      string color = blancas ? "blancas" : "negras";
+      Console.WriteLine($"{playerName} esta jugando con las {color}");
+      Console.ReadLine();
     }
     static bool Exit()
     {
